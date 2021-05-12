39660924532eeb05238e5735f9c0e7d5e6a70fce
Pablo <anaconda_eunectes@hotmail.com>
2021-05-12 20:59:17 +0200

    Cambio URL environment - Peticiones agregadas: Saludos y Historias

---
+++ src/app/modelos/historia.ts
@@ -0,0 +1,8 @@
+export class Historia {
+    id: number;
+    titulo: string;
+    subtitulo: string;
+    descripcion: string;
+    enUso: string;
+    medios: any[]; // TODO --> Ver como tipar este dato
+}
--- src/app/servicios/api.service.ts
+++ src/app/servicios/api.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 import { Acto } from "../modelos/acto";
+import { Saludo } from "../modelos/saludo";
+import { Historia } from '../modelos/historia';
 import { environment } from "../../environments/environment";
 import { Observable } from 'rxjs';
 
@@ -11,16 +13,36 @@ export class ApiService {
 
   constructor(private http:HttpClient) { }
 
-  baseUrl=environment.baseUrl;
+  /**
+   * Direccion API
+   */
+  baseUrl = environment.baseUrl;
 
   /**
-   * consigue todas las imagenes
+   * Peticion GET que devuelve una promesa con un array de Actos
    */
   async getActos(){
     
     return (await this.http.get<Acto[]>(this.baseUrl + "acts/list.php").toPromise()).filter(data => data.categoria =="1");
+  }
 
-    
+  /**
+   * Peticion GET que devuelve una promesa con un array de Saludos
+   * @returns 
+   */
+  async getSaludos(){
+
+    return this.http.get<Saludo[]>(this.baseUrl + "greetings/list.php").toPromise();
+  }
+
+
+  /**
+   * Peticion GET que devuelve una promesa con un array de Historias
+   * @returns 
+   */
+  async getHistorias(){
+
+    return this.http.get<Historia[]>(this.baseUrl + "story/list.php").toPromise();
   }
 
   
