66711541fd0d9ece29c7b177ebb03be137e1c8c2
daniblillo <daniel.10.betico@gmail.com>
2021-05-17 22:24:55 +0200

    Agradecimientos, retoques y versiones

--- src/app/componentes/agradecimientos/agradecimientos.page.ts
+++ src/app/componentes/agradecimientos/agradecimientos.page.ts
@@ -19,41 +19,49 @@ export class AgradecimientosPage implements OnInit {
 
   public agradecimientos = [{
     name: 'Daniel García Villalba',
+    dispositivo: 'Aplicación móvil',
     img: './../../assets/images/agradecimientos/danielgarcia.jpg',
     truncating : true
   },
   {
     name: 'Pedro Francisco Arévalo Alcaide',
+    dispositivo: 'Aplicación móvil',
     img: './../../assets/images/saludos/madreyvonne.jpg',
     truncating : true
   },
   {
     name: 'Pablo Gutiérrez Martínez',
+    dispositivo: 'Aplicación móvil',
     img: './../../assets/images/saludos/madreyvonne.jpg',
     truncating : true
   },
   {
     name: 'José Francisco Ruso Álvarez',
+    dispositivo: 'Panel de administración',
     img: './../../assets/images/agradecimientos/ruso.jpg',
     truncating : true
   },
   {
     name: 'Miguel Alcántara Montero',
+    dispositivo: 'Panel de administración',
     img: './../../assets/images/saludos/madreyvonne.jpg',
     truncating : true
   },
   {
     name: 'Rafael Ramírez Román',
+    dispositivo: 'Panel de administración',
     img: './../../assets/images/saludos/madreyvonne.jpg',
     truncating : true
   },
   {
     name: 'Francisco José Caro Carazo',
+    dispositivo: 'Panel de administración',
     img: './../../assets/images/saludos/madreyvonne.jpg',
     truncating : true
   },
   {
     name: 'Manuel Torres Molina',
+    dispositivo: 'Tutor docente',
     img: './../../assets/images/agradecimientos/manolo.jpg',
     truncating : true
   },
