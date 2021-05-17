7857ba42c738920ed0764882f755bfea24555064
daniblillo <daniel.10.betico@gmail.com>
2021-05-17 21:08:44 +0200

    Agradecimientos y correcciones

--- src/app/componentes/agradecimientos/agradecimientos.page.ts
+++ src/app/componentes/agradecimientos/agradecimientos.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -16,5 +16,47 @@ export class AgradecimientosPage implements OnInit {
   atras(){
     this.router.navigateByUrl('home');
   }
+
+  public agradecimientos = [{
+    name: 'Daniel García Villalba',
+    img: './../../assets/images/agradecimientos/danielgarcia.jpg',
+    truncating : true
+  },
+  {
+    name: 'Pedro Francisco Arévalo Alcaide',
+    img: './../../assets/images/saludos/madreyvonne.jpg',
+    truncating : true
+  },
+  {
+    name: 'Pablo Gutiérrez Martínez',
+    img: './../../assets/images/saludos/madreyvonne.jpg',
+    truncating : true
+  },
+  {
+    name: 'José Francisco Ruso Álvarez',
+    img: './../../assets/images/agradecimientos/ruso.jpg',
+    truncating : true
+  },
+  {
+    name: 'Miguel Alcántara Montero',
+    img: './../../assets/images/saludos/madreyvonne.jpg',
+    truncating : true
+  },
+  {
+    name: 'Rafael Ramírez Román',
+    img: './../../assets/images/saludos/madreyvonne.jpg',
+    truncating : true
+  },
+  {
+    name: 'Francisco José Caro Carazo',
+    img: './../../assets/images/saludos/madreyvonne.jpg',
+    truncating : true
+  },
+  {
+    name: 'Manuel Torres Molina',
+    img: './../../assets/images/agradecimientos/manolo.jpg',
+    truncating : true
+  },
+  ];
 
 }
